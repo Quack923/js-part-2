4005b021d3e67d1a0df38db9b4c9ccfaf6806e4e
Quack923 <vanya.pich@gmail.com>
2026-02-20 19:08:19 +1000

    hw 20.02

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 import { renderComments } from "./modules/renderStudents.js";
 import { comments } from "./modules/arrComments.js";
 export const commentsList = document.querySelector(".comments"); 
-const nameInput = document.querySelector(".add-form-name");
-const commentInput = document.querySelector(".add-form-text");
-const addButton = document.querySelector(".add-form-button");
+import {addButton} from "./modules/validateForm.js";
 
-addButton.addEventListener("click", () => {
-    if (nameInput.value.trim() === "" || commentInput.value.trim() === "") {
-        alert("Заполните все поля!");
-        return;
-    }
-
-    const now = new Date();
-    const dateTime = now.toLocaleString().slice(0, -3);
-
-    comments.push({
-        name: nameInput.value,
-        date: dateTime,
-        text: commentInput.value,
-        likesCount: 0,
-        isLiked: false,
-    });
-
-    nameInput.value = "";
-    commentInput.value = "";
-
-    renderComments();
-});
+renderComments();
